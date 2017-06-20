e3751bf1aee20df4a0f9a1be3945b408e46cdd51
David Goss <davidagoss@gmail.com>
2017-06-20 06:09:01 -0400

    initial import

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+var gulp           = require('gulp')
+var rename         = require('gulp-rename')
+var sass           = require('gulp-sass')
+var nunjucksRender = require('gulp-nunjucks-render')
+var rollup         = require('rollup-stream')
+var source         = require('vinyl-source-stream')
+var resolve        = require('rollup-plugin-node-resolve')
+var commonjs       = require('rollup-plugin-commonjs')
+var connect        = require('gulp-connect')
+var watch          = require('gulp-watch')
+
+gulp.task('html', function(){
+  return gulp.src('views/*.njk')
+    .pipe(nunjucksRender({
+      path: [__dirname, 'views/'] // String or Array
+    }))
+    .pipe(rename({
+      extname: '.html'
+    }))
+    .pipe(gulp.dest('dist'));
+})
+
+gulp.task('scripts', function(){
+  return rollup({
+      entry: './javascripts/app.js',
+      format: 'iife',
+      moduleName: 'site',
+      external: [
+        'bootstrap'
+      ],
+      globals: {
+        bootstrap: 'bootstrap'
+      },
+      plugins: [
+        resolve({ jsnext: true, main: true }),
+        commonjs()
+      ]
+    })
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./dist/js/app.js'));
+})
+
+gulp.task('fonts', function(){
+  return gulp.src([
+    './node_modules/font-awesome/fonts/fontawesome-webfont.*',
+    './node_modules/typeface-arimo/files/arimo-latin-*',
+    './node_modules/typeface-overpass/files/overpass-latin-*'
+  ])
+    .pipe(gulp.dest('dist/resources/fonts/'));
+})
+
+gulp.task('styles', function(){
+  return gulp.src('./stylesheets/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/resources/css'));
+})
+
+gulp.task('webserver', function() {
+  connect.server({
+    livereload: true,
+    root: ['.', 'dist']
+  });
+});
+
+gulp.task('livereload', function() {
+  gulp.src(['dist/**/*'])
+    .pipe(watch('dist/**/*'))
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./javascripts/**/*.js', ['scripts']);
+  gulp.watch('./stylesheets/**/*.scss', ['styles']);
+  gulp.watch('./views/**/*.njk', ['html']);
+});
+
+gulp.task('default', ['html', 'scripts', 'styles', 'fonts', 'webserver', 'livereload', 'watch'])
+
---
+++ javascripts/app.js
@@ -0,0 +1,5 @@
+var $ = require('jquery')
+window.$ = $
+require('bootstrap')
+
+console.log('This is a test script')
